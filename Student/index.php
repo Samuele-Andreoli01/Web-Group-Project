882ed72e77f799e8575d8635cecd3aad08e38ce5
Marek <21010520@hope.ac.uk>
2024-01-04 14:27:55 +0000

    I uploaded code created by Nabil no changes made at this stage.

---
+++ Student/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.html" ><img src="logo.png" alt="University Logo"></a></li>
+            <li><a href="courses.html">Courses</a></li>
+            <li><a href="aboutus.html">About us</a></li>
+			<li><a href="contactus.html">Contact us</a></li>
+		<ul>
+		
+		<ul>
+		
+				<h1>University of </h1>
+        <ul>
+		
+    </nav>
+	
+  
+    <header>
+        <img src="img1.jpg" alt="HomePage">
+		<h1>HomePage </h1>
+		
+    </header>
+    <section>
+        <h2>Timetable</h2>
+        <p>  </p>
+    </section>
+    <section>
+        <h2>News</h2>
+        <p>  <br>
+</p>
+
+    </section>
+	<section>
+
+        <h2>Address</h2>
+        <p>XXXXXXXXXX</p>
+    </section>
+    <script src="script.js"></script>
+</body>
+</html>
